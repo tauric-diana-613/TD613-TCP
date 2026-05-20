5de45affc2efbe2784ed7fc9a767bea87d8849d5
tauric-diana-613 <timstephencello@gmail.com>
2026-05-19 22:49:44 -0400

    hush: initialize alien console surface

--- app/adversarial-bench.js
+++ app/adversarial-bench.js
@@ -1,8 +1,12 @@
 import './hush-simple-path.js';
 import * as bench from './adversarial-bench.mjs';
 import { initHushInvisibleShell } from './hush-invisible-shell.js';
+import { initHushAlienConsole } from './hush-alien-console.js';
 
 if (typeof window !== 'undefined') window.__TD613_HUSH_BENCH__ = bench;
-if (typeof document !== 'undefined') initHushInvisibleShell(document, bench);
+if (typeof document !== 'undefined') {
+  initHushInvisibleShell(document, bench);
+  initHushAlienConsole(document, bench);
+}
 
 export * from './adversarial-bench.mjs';
