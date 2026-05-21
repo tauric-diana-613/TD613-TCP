233e626d3080006cf9fb1d979d2c1f8da5a6cb72
tauric-diana-613 <timstephencello@gmail.com>
2026-05-21 17:32:01 -0400

    hush: boot Phase 31 customizer forge

--- app/hush-customizer-card-fields-boot.js
+++ app/hush-customizer-card-fields-boot.js
@@ -1,7 +1,9 @@
 import { initHushCustomizerCardFields } from './hush-customizer-card-fields.js';
+import { initCustomizerForge } from './hush-customizer-forge.js';
 
 function boot() {
   initHushCustomizerCardFields(document, window.__TD613_HUSH_BENCH__ || {});
+  initCustomizerForge(document);
 }
 
 if (typeof window !== 'undefined' && typeof document !== 'undefined') {
