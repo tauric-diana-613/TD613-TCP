7ff9918bcfe216b4a7b8e06b71c2d987627a221a
tauric-diana-613 <timstephencello@gmail.com>
2026-05-26 17:54:47 -0400

    fix(hush): remove overlapping profile overlays

--- app/chamber-bootstrap.js
+++ app/chamber-bootstrap.js
@@ -127,26 +127,10 @@
     pr79Boot.type = 'module';
     pr79Boot.src = './hush-pr79-coverage-floor.js?v=' + (V.hushPr79 || V.hushPr78 || V.hushPatch38 || V.main || '');
     document.head.appendChild(pr79Boot);
-    var pr81Boot = document.createElement('script');
-    pr81Boot.type = 'module';
-    pr81Boot.src = './hush-pr81-mobile-profile-carousel.js?v=' + (V.hushPr81 || V.hushPr79 || V.hushPatch38 || V.main || '');
-    document.head.appendChild(pr81Boot);
     var pr82Boot = document.createElement('script');
     pr82Boot.type = 'module';
-    pr82Boot.src = './hush-pr82-mobile-state-hotfix.js?v=' + (V.hushPr82 || V.hushPr81 || V.hushPatch38 || V.main || '');
+    pr82Boot.src = './hush-pr82-mobile-state-hotfix.js?v=' + (V.hushPr82 || V.hushPr76 || V.hushPatch38 || V.main || '');
     document.head.appendChild(pr82Boot);
-    var pr83Boot = document.createElement('script');
-    pr83Boot.type = 'module';
-    pr83Boot.src = './hush-pr83-profile-grid-mobile-fix.js?v=' + (V.hushPr83 || V.hushPr82 || V.hushPr81 || V.hushPatch38 || V.main || '');
-    document.head.appendChild(pr83Boot);
-    var pr84Boot = document.createElement('script');
-    pr84Boot.type = 'module';
-    pr84Boot.src = './hush-pr84-authorship-metrics-plus.js?v=' + (V.hushPr84 || V.hushPr83 || V.hushPr82 || V.hushPatch38 || V.main || '');
-    document.head.appendChild(pr84Boot);
-    var pr85Boot = document.createElement('script');
-    pr85Boot.type = 'module';
-    pr85Boot.src = './hush-pr85-authorship-scroll-tuner.js?v=' + (V.hushPr85 || V.hushPr84 || V.hushPr83 || V.hushPatch38 || V.main || '');
-    document.head.appendChild(pr85Boot);
   }
 
   var srcs = [
