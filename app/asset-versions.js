9f4ecb53bd7e454f4e974860d9520360b57f7556
tauric-diana-613 <timstephencello@gmail.com>
2026-05-31 00:03:40 -0400

    chore(hush): version stylometry ontology release guard

--- app/asset-versions.js
+++ app/asset-versions.js
@@ -28,6 +28,7 @@
     hushPr98: '202605281855',
     hushPr99: '202605281855',
     hushPr104: '202605301905',
+    hushPr106: '202605302105',
     hushPhase39: '202605301720'
   };
   var V = window.TD613_ASSET_VERSIONS;
