5bfa9824328bf4af82d376c4e278be8e90bafc89
tauric-diana-613 <timstephencello@gmail.com>
2026-05-31 22:24:52 -0400

    fix(hush): load PR117 generating watchdog

--- app/chamber-bootstrap.js
+++ app/chamber-bootstrap.js
@@ -85,6 +85,7 @@
     appendScript('./hush-pr96-cockpit-stabilizer.js?v=' + (V.hushPr96 || V.hushPr95 || V.hushPatch38 || V.main || ''));
     appendScript('./hush-pr97-mobile-drawer-watchdog.js?v=' + (V.hushPr97 || V.hushPr96 || V.hushPatch38 || V.main || ''));
     appendScript('./hush-pr104-rigor-amplifier.js?v=' + (V.hushPr104 || V.hushPr97 || V.hushPatch38 || V.main || ''));
+    appendScript('./hush-pr117-generating-watchdog-receipt.js?v=' + (V.hushPr117 || V.hushPr116 || V.hushPr115 || V.hushPr114 || V.hushPatch38 || V.main || ''));
     appendScript('./hush-pr106-stylometry-ontology-release-guard.js?v=' + (V.hushPr106 || V.hushPr104 || V.hushPatch38 || V.main || ''));
     appendScript('./hush-pr107-selector-recovery.js?v=' + (V.hushPr107 || V.hushPr106 || V.hushPatch38 || V.main || ''));
     appendModule('./hush-pr108-live-state-recovery-bridge.js?v=' + (V.hushPr108 || V.hushPr107 || V.hushPatch38 || V.main || ''));
