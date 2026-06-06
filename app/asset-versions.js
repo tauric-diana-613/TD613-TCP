7c139ff3dcdfdc3a58c2f98036962bb9890dbfbc
tauric-diana-613 <timstephencello@gmail.com>
2026-06-06 00:51:32 -0400

    Bump Hush PR141 asset version

--- app/asset-versions.js
+++ app/asset-versions.js
@@ -1,14 +1,15 @@
 (function () {
   window.TD613_ASSET_VERSIONS = {
-    styles:      '202604192040',
-    data:        '202605281815',
+    styles: '202604192040',
+    data: '202605281815',
     diagnostics: '202604230045',
-    engine:      '202605281855',
-    fixtures:    '202604230045',
-    receipt:     '202605010945',
-    main:        '202604230045',
-    aperture:    '202605281835',
-    chrome:      '202606010430',
+    engine: '202605281855',
+    fixtures: '202604230045',
+    receipt: '202605010945',
+    main: '202604230045',
+    aperture: '202605281835',
+    chrome: '202606010430',
     copyHygiene: '202606011235',
     hushAlienConsole: '202605250326',
-    hush
+    hushFieldInstrument: '202605250326',
+    hushMobileViewport
