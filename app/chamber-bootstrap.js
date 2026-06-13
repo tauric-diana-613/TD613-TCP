267b7194c3612698a27f9799eb96a2c773b886ad
tauric-diana-613 <timstephencello@gmail.com>
2026-06-13 13:51:24 -0400

    fix(hush): let sample persistence bind

--- app/chamber-bootstrap.js
+++ app/chamber-bootstrap.js
@@ -32,7 +32,7 @@
 
   function installHushPhase31BindGuard() {
     if (window.__TD613_HUSH_PHASE31_BIND_GUARD__) return;
-    window.__TD613_HUSH_PHASE31_BIND_GUARD__ = 'chamber-bootstrap/v2-no-tab-guard';
+    window.__TD613_HUSH_PHASE31_BIND_GUARD__ = 'chamber-bootstrap/v3-boot-listener-only';
     var ids = {
       hushPhase31LogSampleBtn: { click: true },
       hushPhase31Undo: { click: true },
@@ -42,14 +42,19 @@
       hushPhase31ResetCustomizer: { click: true },
       hushVoiceReferenceSamplesSaved: { input: true }
     };
+    var bootListenerPattern = /(logSample\(doc\)|undoSample\(doc\)|openSaveModal\(doc\)|addToStudio\(doc\)|resetCustomizer\(doc\)|updateWordCounter\(doc\)|modal\.hidden\s*=\s*true|input\.click\(\)|exportMask\(doc\))/;
     var originalAdd = EventTarget.prototype.addEventListener;
     EventTarget.prototype.addEventListener = function (type, listener, options) {
       try {
         var id = this && this.id;
         if (id && ids[id] && ids[id][type] && this.dataset) {
-          var key = 'td613Phase31' + type.charAt(0).toUpperCase() + type.slice(1) + 'Bound';
-          if (this.dataset[key] === 'true') return;
-          this.dataset[key] = 'true';
+          var source = '';
+          try { source = Function.prototype.toString.call(listener); } catch (error) { source = ''; }
+          if (bootListenerPattern.test(source)) {
+            var key = 'td613Phase31' + type.charAt(0).toUpperCase() + type.slice(1) + 'BootBound';
+            if (this.dataset[key] === 'true') return;
+            this.dataset[key] = 'true';
+          }
         }
       } catch (error) {}
       return originalAdd.call(this, type, listener, options);
