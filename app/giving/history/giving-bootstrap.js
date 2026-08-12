db8595594109954f914ad3f1664e5dffa7bbe566
tauric-diana-613 <timstephencello@gmail.com>
2026-08-12 14:01:54 -0400

    Load Research Dossier hover help

--- app/giving/history/giving-bootstrap.js
+++ app/giving/history/giving-bootstrap.js
@@ -3,3 +3,4 @@ import './giving-export-menu.js?v=20260812-5';
 await import('./giving-app.js?v=20260812-5');
 await import('./giving-search-controls.js?v=20260812-5');
 await import('./giving-campaign-tools.js?v=20260812-5');
+await import('./giving-dossier-help.js?v=20260812-5');
