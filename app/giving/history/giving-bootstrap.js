f6112da0746c689c2ed6ef112bcd8c70df5178df
tauric-diana-613 <timstephencello@gmail.com>
2026-08-12 14:45:27 -0400

    Refresh Research Dossier help module

--- app/giving/history/giving-bootstrap.js
+++ app/giving/history/giving-bootstrap.js
@@ -3,4 +3,4 @@ import './giving-export-menu.js?v=20260812-5';
 await import('./giving-app.js?v=20260812-5');
 await import('./giving-search-controls.js?v=20260812-5');
 await import('./giving-campaign-tools.js?v=20260812-5');
-await import('./giving-dossier-help.js?v=20260812-6');
+await import('./giving-dossier-help.js?v=20260812-7');
